913b9f0b56e32c3a3eda5de1ab4e9b68720cc0c0
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-21 21:07:36 +0800

    Delete exercises directory

--- exercises/sinc/sinc_python.py
+++
@@ -1,6 +0,0 @@
-from math import sin
-
-def sinc_kernel(x):
-    if -0.01 < x < 0.01:
-        return 1.0
-    return sin(x) / x
--- exercises/sinc/sinc_solution.pyx
+++
@@ -1,6 +0,0 @@
-from libc.math cimport sin
-
-cpdef double sinc_kernel(double x):
-    if -0.01 < x < 0.01:
-        return 1.0
-    return sin(x) / x
